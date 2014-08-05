1b151df4c13ce1ca85b1e0cbdc6d4e20ccfd9b2d
lu446529842 <446529842@qq.com>
2014-08-05 20:53:05 +0800

    完成基本的dao接口设计

--- BaseDao/BaseDao/src/com/mls/common/dao/BaseDao.java
+++ BaseDao/BaseDao/src/com/mls/common/dao/BaseDao.java
@@ -1,11 +1,173 @@
 package com.mls.common.dao;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.mls.common.bean.BaseEntity;
+import com.mls.common.util.PageInfo;
+import com.mls.common.util.PagerControl;
 
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 
 public abstract class BaseDao<T extends Serializable, M extends BaseEntity> extends  SqlSessionDaoSupport implements IDao<T, M>{
+
+	private Set<String> entityFields = new HashSet();
+	public Map<String, Object> getMapParams(M entity, PageInfo pageInfo, String whereSql, String orderBySql) {
+	    Map map = new HashMap();
+	    if (null != entity) map.put("entity", entity);
+	    if (null != pageInfo) map.put("pageInfo", pageInfo);
+	    if (null != whereSql) map.put("whereSql", whereSql);
+	    if (null != orderBySql) map.put("orderBy", orderBySql);
+	    return map;
+	}
+	
+	public String getOrderBySql(PageInfo pageInfo) {
+	    if (pageInfo == null) return null;
+	    StringBuilder sb = new StringBuilder();
+	    if ((!Strings.isNullOrEmpty(pageInfo.getOrderField())) && (this.entityFields.contains(pageInfo.getOrderField()))) {
+	      sb.append("order by ").append(pageInfo.getOrderField()).append(" ").append("desc".equals(pageInfo.getOrderDirection()) ? "desc" : "asc");
+	    }
+	    
+	    return sb.toString();
+	}
+
+
+	@Override
+	public int insert(M entity) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(T pk) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteByEntity(M entity) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(M entity) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public M getEntityById(T pk) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<M> getEntityByIds(List<T> paramList) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<T, M> getEntityByIds(List<T> paramList, String whereSql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public M getEntityByObj(M entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public M getEntityByIdUseDB(T pk) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public M getEntityByObj(M entity, String whereSql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCountByObj(M entity) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCountByObj(M entity, String whereSql) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public PagerControl<M> getPagerByObj(M entity, PageInfo pageInfo,
+			String whereSql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PagerControl<M> getPagerByObj(M entity, PageInfo pageInfo,
+			String whereSql, String orderBySql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<M> getAllEntityObj() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<M> getListByObj(M entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<M> getListByObj(M entity, String whereSql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<M> getListByObj(M entity, String whereSql, String orderBySql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<M> getListByObj(M entity, PageInfo pageInfo, String whereSql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<M> getListByObj(M entity, PageInfo pageInfo, String whereSql,
+			String orderBySql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<M> getListByObjSortByMultiField(M entity, PageInfo pageInfo,
+			String whereSql, String orderBySql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- BaseDao/BaseDao/src/com/mls/common/dao/IDao.java
+++ BaseDao/BaseDao/src/com/mls/common/dao/IDao.java
@@ -11,87 +11,50 @@ import com.mls.common.bean.BaseEntity;
 /**
  * 丰富有的抽象base dao
  * @author yuzhe
- *
  * @param <T>
  * @param <M>
  */
 public interface IDao<T extends Serializable, M extends BaseEntity> {
 
-	public  int insert(M paramM);
+	public  int insert(M entity);
 
-	public  int delete(T paramT);
+	public  int delete(T pk);
 
-	public  int deleteByEntity(M paramM);
+	public  int deleteByEntity(M entity);
 
-	public  int update(M paramM);
+	public  int update(M entity);
 
-	public  M getEntityById(T paramT);
-
-	public  M getEntityById(T paramT, boolean paramBoolean);
+	public  M getEntityById(T pk);
 
 	public List<M> getEntityByIds(List<T> paramList);
 
-	public  List<M> getEntityByIds(List<T> paramList,boolean paramBoolean);
+	public  Map<T, M> getEntityByIds(List<T> paramList,String whereSql);
 
-	public  Map<T, M> getEntityByIds(List<T> paramList,String paramString);
+	public  M getEntityByObj(M entity);
 
-	public  Map<T, M> getEntityByIds(List<T> paramList,String paramString, boolean paramBoolean);
+	public  M getEntityByIdUseDB(T pk);
 
-	public  M getEntityByObj(M paramM);
+	public  M getEntityByObj(M entity, String whereSql);
 
-	public  M getEntityByObj(M paramM, boolean paramBoolean);
+	public  int getCountByObj(M entity);
 
-	public  M getEntityByIdUseDB(T paramT);
+	public  int getCountByObj(M entity, String whereSql);
 
-	public  M getEntityByIdUseDB(T paramT, boolean paramBoolean);
+	public  PagerControl<M> getPagerByObj(M entity,PageInfo pageInfo, String whereSql);
 
-	public  M getEntityByObj(M paramM, String paramString);
-
-	public  M getEntityByObj(M paramM, String paramString,boolean paramBoolean);
-
-	public  int getCountByObj(M paramM);
-
-	public  int getCountByObj(M paramM, boolean paramBoolean);
-
-	public  int getCountByObj(M paramM, String paramString);
-
-	public  int getCountByObj(M paramM, String paramString,boolean paramBoolean);
-
-	public  PagerControl<M> getPagerByObj(M paramM,PageInfo paramPageInfo, String paramString);
-
-	public  PagerControl<M> getPagerByObj(M paramM,PageInfo paramPageInfo, String paramString, boolean paramBoolean);
-
-	public  PagerControl<M> getPagerByObj(M paramM,PageInfo paramPageInfo, String paramString1, String paramString2);
-
-	public  PagerControl<M> getPagerByObj(M paramM,PageInfo paramPageInfo, String paramString1, String paramString2,boolean paramBoolean);
+	public  PagerControl<M> getPagerByObj(M entity,PageInfo pageInfo, String whereSql, String orderBySql);
 
 	public  List<M> getAllEntityObj();
 
-	public  List<M> getAllEntityObj(boolean paramBoolean);
+	public  List<M> getListByObj(M entity);
 
-	public  List<M> getListByObj(M paramM);
+	public  List<M> getListByObj(M entity, String whereSql);
 
-	public  List<M> getListByObj(M paramM, boolean paramBoolean);
+	public  List<M> getListByObj(M entity, String whereSql,String orderBySql);
 
-	public  List<M> getListByObj(M paramM, String paramString);
+	public  List<M> getListByObj(M entity, PageInfo pageInfo,String whereSql);
 
-	public  List<M> getListByObj(M paramM, String paramString,boolean paramBoolean);
+	public  List<M> getListByObj(M entity, PageInfo pageInfo,String whereSql, String orderBySql);
 
-	public  List<M> getListByObj(M paramM, String paramString1,String paramString2);
-
-	public  List<M> getListByObj(M paramM, String paramString1,String paramString2, boolean paramBoolean);
-
-	public  List<M> getListByObj(M paramM, PageInfo paramPageInfo,String paramString);
-
-	public  List<M> getListByObj(M paramM, PageInfo paramPageInfo,String paramString, boolean paramBoolean);
-
-	public  List<M> getListByObj(M paramM, PageInfo paramPageInfo,String paramString1, String paramString2);
-
-	public  List<M> getListByObj(M paramM, PageInfo paramPageInfo,String paramString1, String paramString2, boolean paramBoolean);
-
-	public  List<M> getListByObjSortByMultiField(M paramM,PageInfo paramPageInfo, String paramString1, String paramString2);
-
-	public  List<M> getListByObjSortByMultiField(M paramM,PageInfo paramPageInfo, String paramString1, String paramString2,boolean paramBoolean);
-
-	public  boolean deleteCacheByPk(T paramT);
+	public  List<M> getListByObjSortByMultiField(M entity,PageInfo pageInfo, String whereSql, String orderBySql);
 }
